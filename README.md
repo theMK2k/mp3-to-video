3847f4c964af84eb3b03a71c871718412495214a
MK2k <gitlab@mk2k.net>
2022-05-15 23:01:20 +0200

    e2e conversion of mp3 to mp4

---
+++ README.md
@@ -0,0 +1,17 @@
+# MP3 to Video
+
+Convert mp3 files to mp4 video with a simple image generated from the filename.
+
+## Usage
+
+Feed a file as `--src` and it will be converted.
+
+Feed a directory as `--src` and all the files within it will be converted.
+
+Use `-v` or `--verbose` and you get some verbose logging.
+
+Example:
+
+```bash
+node mp3-to-video.js --src /path/to/file/or/directory
+```
--- mp3-to-video.js
+++ mp3-to-video.js
@@ -1,7 +1,11 @@
+const path = require("path");
+
 const logger = require("loglevel");
 const commandLineArgs = require("command-line-args");
 const nodeHtmlToImage = require("node-html-to-image");
 const fsExtra = require("fs-extra");
+const ffmpegPath = require("ffmpeg-static");
+const execa = require("execa");
 
 const optionDefinitions = [
   { name: "verbose", alias: "v", type: Boolean },
@@ -21,9 +25,11 @@ if (!options.src) {
   process.exit(0);
 }
 
-async function textToPng(text) {
+async function textToPng(text, fullPath) {
+  logger.log("  running textToPng...");
+
   await nodeHtmlToImage({
-    output: "./image.png",
+    output: fullPath,
     html: `
 <html>
     <head>
@@ -40,6 +46,83 @@ async function textToPng(text) {
   });
 }
 
+async function processFiles(cwd, files) {
+  for (let file of files) {
+    const fullPath = path.join(cwd, file);
+
+    logger.log("fullPath:", fullPath);
+
+    const stat = await fsExtra.stat(fullPath);
+    if (stat.isDirectory()) {
+      logger.info("skipping directory", file);
+      continue;
+    }
+
+    const extension = path.extname(fullPath).toLowerCase();
+    if (![".mp3"].find((ext) => ext === extension)) {
+      logger.info("skipping non-mp3 file", file);
+      continue;
+    }
+
+    await processFile(cwd, file);
+  }
+}
+
+async function runFFMPEG(audioFullPath, pngFullPath, outFullPath) {
+  logger.log("  runFFMPEG ffmpeg location:", ffmpegPath);
+
+  if (await fsExtra.exists(outFullPath)) {
+    fsExtra.rm(outFullPath, { force: true });
+  }
+
+  // ffmpeg -loop 1 -framerate 2 -i byu-utah.png -i nfl.mp3 -c:v libx264 -preset medium -tune stillimage -crf 18 -c:a copy -shortest -pix_fmt yuv420p -s:v 1920x1080 byu-utah.mp4
+  const { stdout } = await execa(ffmpegPath, [
+    "-loop",
+    "1",
+    "-framerate",
+    "2",
+    "-i",
+    pngFullPath,
+    "-i",
+    audioFullPath,
+    "-c:v",
+    "libx264",
+    "-preset",
+    "medium",
+    "-tune",
+    "stillimage",
+    "-crf",
+    "18",
+    "-c:a",
+    "copy",
+    "-shortest",
+    "-pix_fmt",
+    "yuv420p",
+    "-s:v",
+    "1920x1080",
+    outFullPath,
+  ]);
+}
+
+async function processFile(cwd, file) {
+  logger.info("processing file", file);
+
+  const nameWithoutExtension = path.parse(file).name;
+
+  logger.log("  nameWithoutExtension:", nameWithoutExtension);
+
+  await textToPng(
+    nameWithoutExtension,
+    path.join(cwd, `${nameWithoutExtension}.png`)
+  );
+
+  await runFFMPEG(
+    path.join(cwd, file),
+    path.join(cwd, `${nameWithoutExtension}.png`),
+    path.join(cwd, `${nameWithoutExtension}.mp4`)
+  );
+}
+
 (async () => {
   try {
     const src = options.src;
@@ -54,6 +137,15 @@ async function textToPng(text) {
     //   await textToPng(
     //     "Pete Cannon - 90's beats (Band of gold) - Pete Cannon - 90's beats (band of gold) - 01 You fakin it buddy!"
     //   );
+
+    const cwd = srcStat.isDirectory() ? src : path.dirname(src);
+    logger.log("cwd:", cwd);
+
+    const files = srcStat.isFile() ? [src] : await fsExtra.readdir(src);
+
+    logger.log("files:", files);
+
+    await processFiles(cwd, files);
   } catch (err) {
     logger.error("Exception:", err);
   }
